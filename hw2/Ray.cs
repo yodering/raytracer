9824d35e65bc3585e57738d88e1ba59a5386520c
David Yoder <david.yoder@live.com>
2025-09-30 20:23:07 -0400

    ray.cs class done

--- hw2/Ray.cs
+++ hw2/Ray.cs
@@ -2,7 +2,28 @@ using System;
 
 
 namespace Raytracer.HW2;
+
 public class Ray
 {
 
+    public Vector Origin { get; set; }
+    public Vector Direction { get; set; }
+
+    public Ray()
+    {
+        Origin = new Vector();
+        Direction = new Vector(0f, 0f, 1f);
+    }
+
+    public Ray(Vector origin, Vector direction)
+    {
+        // checks for validity
+        Origin = origin ?? throw new ArgumentNullException(nameof(origin));
+        Direction = direction ?? throw new ArgumentNullException(nameof(direction));
+    }
+
+    public Vector At(float t)
+    {
+        return Origin + Direction * t;
+    }
 }
